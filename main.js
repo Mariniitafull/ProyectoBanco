1ce1d4e257a7633cd13035ed6d7324a5c3f6eef0
Tarde <marinafullferrer@gmail.com>
2022-06-10 19:48:31 +0200

    Primer commit

---
+++ main.js
@@ -0,0 +1,41 @@
+
+const gestor1 = {
+    id: 1,
+    usuario: 'gestor1',
+    passwrod: 'gestor1',
+    correo: 'gestor1@gmail.com'
+};
+const gestor2 = {
+    id: 2,
+    usuario: 'gestor2',
+    passwrod: 'gestor2',
+    correo: 'gestor2@gmail.com'
+};
+
+const cliente1 = {
+    id: 1,
+    id_gestor: 1  ,
+    usuario: 'cliente1' ,
+    password: 'cliente1',
+    correo: 'cliente1@gmail.com',
+    saldo: 100.50
+}
+
+const cliente2 = {
+    id: 2,
+    id_gestor: 1 ,
+    usuario: 'cliente2',
+    password: 'cliente2',
+    correo: 'cliente2@gmail.com',
+    saldo: 100.50
+}
+
+// const mensaje1 = { //id de gestores
+//     id: 1,
+//     id_origen: ,
+//     id_destino: ,
+//     texto: char(140),
+//     fecha: datetime
+// }
+console.log(gestor2);
+console.table(cliente2);
