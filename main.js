ca86f03fcbaf3ccaa1e76b3ee596912991cc5591
Tarde <marinafullferrer@gmail.com>
2022-06-15 20:28:42 +0200

    Ejercicio 4.2

--- main.js
+++ main.js
@@ -8,51 +8,19 @@
 Escribe un programa que almacene los objetos creados en el ejercio anterior del proyecto dentro de un array (un array por cada modelo de datos). A continuación, recorre cada uno de los arrays y muestra todas propiedades..
 *///no me vale ese enunciado
 
-function mostrarGestor(gestor) {
-    console.log(`id: ${gestor.id}`);
-    console.log(`usuario: ${gestor.usuario}`);
-    console.log(`password: ${gestor.password}`);
-    console.log(`correo: ${gestor.correo}`);
-    console.log('-----');
-}
-function mostrarGestores(gestores) {
-
-//gestores es un array
-//gestor es un objeto con las propiedades: id, usuario, password, email
-console.log('GESTORES');
-for (const gestor of gestores){
-    mostrarGestor(gestor);
-}
-}
-function mostrarCliente(cliente) {
-    console.log(`id: ${cliente.id}`);
-    console.log(`id_gestor: ${cliente.id_gestor}`);
-    console.log(`usuario: ${cliente.usuario}`);
-    console.log(`password: ${cliente.password}`);
-    console.log(`correo: ${cliente.correo}`);
-    console.log(`saldo: ${cliente.saldo}`);
-    console.log('-----');
-}
-
-function mostrarClientes(clientes) {
-    console.log('--- CLIENTES ---');
-    for (const cliente of clientes) {
-        mostrarCliente(cliente);
-    }
-}
 
 
-const gestores = generarGestores();
-console.log(gestores);
+// const gestores = generarGestores();
+// console.log(gestores);
 
-const gestoresJSON = JSON.stringify(gestores);
-console.log(gestoresJSON);
-console.log(gestoresJSON.length);
+// const gestoresJSON = JSON.stringify(gestores);
+// console.log(gestoresJSON);
+// console.log(gestoresJSON.length);
 
-const clientes = generarClientes();
-const cliente1 = clientes[0];
-const cliente1JSON = JSON.stringify(cliente1);
-console.log(cliente1JSON);
+// const clientes = generarClientes();
+// const cliente1 = clientes[0];
+// const cliente1JSON = JSON.stringify(cliente1);
+// console.log(cliente1JSON);
    
 
 /*escribe un programa que realice la conversión a JSON del array (y viceversa)
@@ -66,6 +34,100 @@ creado en el ejercicio anterior del proyecto*/
 
 // const cliente1JSON = JSON.stringify(cliente1);
 // console.log(cliente1JSON);
+//////////////////////////////////////////////////////////////////////////
+/*
+    Realizar una petición a http://localhost:8085/ok y mostrar la respuesta por pantalla
+*/
 
+
+// las propiedades del objeto;las opciones son: url,metodo,body, cabeceras
+// const server = 'localhost:8085';
+
+
+
+//lo de abajo es lo mismo q esto:
+// const server = 'localhost:8085';
+
+// const opciones = {
+//     url: `http://${server}/ok`,
+//     metodo: 'GET'
+// };
+
+// ajax(opciones, (data) => {
+//     console.log(data);
+// });
+
+
+// const opcionesLogin = {
+//     url: `http://${server}/login/gestor/`,
+//     metodo: 'POST',
+//     body: 'usuario=gestor1&password=gestor1',
+//     cabeceras: {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//     }
+// };
+
+// ajax(opcionesLogin, (data) => {
+//     console.log(data);
+// })
+const server = 'localhost:8085';
+
+const opciones = {
+    url:'http://localhost:8085/ok',
+    metodo: 'GET'  //método http
+};
+
+ajax(opciones, (data) => {
+    console.log(data);
+})
+//-------------------Enviar datos al servidor
+const opcionesLogin ={
+    url: 'http://localhost:8085/login/gestor/',
+    metodo: 'POST',
+    body:'usuario=gestor1&password=gestor1',
+    cabeceras: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+};
+// realiza la petición de login del gestor
+ajax(opcionesLogin, (data) =>{
+    // esta función se ejecuta cuando los datos han sido
+    // devueltos por el servicio
+
+    // convirtiendo el string en formato JSON a objeto de JavaScript
+    const respuesta = JSON.parse(data);
+   // guardamos el token
+    const token = respuesta.data.token;
+
+   // realizamos la solicitud para obtener todos los gestores
+
+   const opcionesObtenerGestores = {
+    url: 'http://localhost:8085/gestores/',
+    metodo: 'GET',
+    cabeceras: {
+         Authorization: `Basic ${token}`
+    }
+}
+
+ajax(opcionesObtenerGestores, (data) => {
+
+    //data tiene toda la información de los gestores en formato string
+    console.log(data);
+
+    // convertir a objeto de JS
+    const respuesta = JSON.parse(data);
+
+    // console.log(respuesta);
+    const gestores = respuesta.data;
+    // console.log(gestores);
+    mostrarGestores(gestores);
+
+    
+});
+});
+
+// obtenerGestores(gestores => {
+//     mostrarGestores(gestores);
+// })
 
 
